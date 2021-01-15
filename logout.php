90a375ffcc8dddbcf56b81774de54a984a99ec4d
LAPTOP-2HM0ARKO\sotow <sotoworld@hotmail.com>
2021-01-15 11:23:54 +0100

    subidaoauthsegundasesionstarunset

--- logout.php
+++ logout.php
@@ -1,8 +1,10 @@
 <?php
 # Si no entiendes esto, primero mira a login.php
-$google_oauth->logout();
+
 # Iniciar sesión (sí, aunque la vamos a destruir, primero se debe iniciar)
 session_start();
+unset($_SESSION['google']);
+unset($_SESSION['usuario']);
 # Después, destruirla
 # Eso va a eliminar todo lo que haya en $_SESSION
 session_destroy();
